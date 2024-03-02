0a79056e1c0538291da1ab00af187fc1b1f7001c
Imelda Yoon <emy13@me.com>
2024-03-02 10:13:02 -0500

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Imelda Yoon on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
